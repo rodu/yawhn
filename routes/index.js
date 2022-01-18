935272f86f892fc772237b8582411bfde8c40f25
rodu <3225111+rodu@users.noreply.github.com>
2022-01-18 13:27:33 +0000

    Implement post action to suspend the computer

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const { exec } = require('child_process');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Yawhn!' });
+});
+
+router.post('/sleep', function (req, res, next) {
+  // Uses shell command to put computer to sleep
+  exec('systemctl suspend', (error) => {
+    if (error) {
+      res.status(500).send({ message: 'Suspend failed', error: true });
+    }
+  });
 });
 
 module.exports = router;
